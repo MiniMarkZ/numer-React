c9159a5a05db5bc118bbe3597f260c10b4a7e5fa
MiniMarkZ <markipg@hotmail.com>
2023-03-25 15:18:53 +0700

    test swagger

--- api/index.js
+++ api/index.js
@@ -4,16 +4,33 @@ var cors = require('cors')
 const app = express();
 const equations = require('./data/equation.json');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
 app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World! this Api numer');
 });
 
+
+/**
+ * @swagger
+ * /Bisection:
+ *   get:
+ *     description: Returns equations for Bisection method
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 app.get('/Bisection', (req, res) => {
     const bisectionEquations = equations.filter(equation => equation.method === 'Bisection');
     res.json(bisectionEquations);
 });
+
+
 
 app.get('/FalsePosition', (req, res) => {
     const falsePositionEquations = equations.filter(equation => equation.method === 'FalsePosition');
