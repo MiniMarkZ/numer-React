e05dc6521251f73f44822d895e5aa4c1cd64d0e8
MiniMarkZ <markirpg@hotmail.com>
2023-04-07 11:50:58 +0700

    tesy v3

--- src/component/regression/tesy.js
+++ src/component/regression/tesy.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Row , Col } from "react-bootstrap";
-import polynomialRegression from 'ml-regression-polynomial';
 import Plottest from "./Plottest";
+import { PolynomialRegressor } from 'regression-multivariate-polynomial';
 
 function Test() {
   const [m, setM] = useState(0);
@@ -12,13 +12,17 @@ function Test() {
   const [havedata , setHavedata] = useState(0);
   const [tablevalue ,setTablevalue] = useState([...Array(n)].map(() => Array(m).fill(0))); // แก้บัค
   const [result, setResult] = useState(null);
-  const [X, setX] = useState(0)
   const [A,setA] = useState()
   const [ypredict,setYpredict] = useState([])
   const [plotx,setPlotx] = useState([])
   const [ploty,setPloty] = useState([])
+  const [degree,setDegree] = useState(0);
+  const [xp,setXp ]= useState([])
+  const [xpredict,setXpredict] = useState([])
 
-  
+  const Setdegree = (e) =>{
+    setDegree(e.target.value)
+  }
   const Setn = (event) => {
     setN(event.target.value);
     setTablevalue(Array.from({ length: event.target.value }, () => new Array(m).fill(""))); // แก้บัค
@@ -27,20 +31,29 @@ function Test() {
     console.log("คำนวณ จบิง")
     console.log(tablevalue)
     calculatetesy()
-    console.log("plotx",plotx)
-    console.log("ploty",ploty)
+    // console.log("plotx",plotx)
+    // console.log("ploty",ploty)
 
   }
 
-  const Setx = (e) => {
-    setX(e.target.value)
-    
+  const Setx = (e,j) => {
+    var xtmp = []
+    console.log(e.target.value ," = " ,j)
+    xtmp = xpredict;
+    console.log("asd",xtmp)
+    xtmp[j] = Number(e.target.value);
+    setXpredict(xtmp)
+    console.log("asd",xpredict)
   }
 
   const Setm = (event) => {
+    const tmpnumber = Number(event.target.value)
+    const newArray = new Array(tmpnumber-1).fill(0);
+    console.log("newa",newArray);
     setM(event.target.value);
-    
+    setXpredict(newArray);
   };
+  
 
   const inputn = (e, i, j) => {
     console.log(`matrix [${i}] [${j}] = ${e.target.value}`)
@@ -66,43 +79,39 @@ function Test() {
   };
    
   const calculatetesy = () => {
-    for(let j = 0 ; j < m-1 ; j++){
-      const x = []
-      const y = []
-      var testdata =[]
-      for (let i = 0; i < n; i++) {
-          const row = tablevalue[i];
-          const xi = parseFloat(row[0]);
-          const yi = parseFloat(row[m-1]);
-          x.push(xi);
-          y.push(yi);
-        }
-        setPlotx(x)
-        setPloty(y)
-
-      const degree = m
-      const regression = new polynomialRegression(x, y, degree);
-      console.log(regression)
-      const result = regression.predict(parseInt(X));
-      setA(regression.coefficients)
-      const regres = regression.coefficients
     
-      setResult(result)
-      console.log("result",result)
-      var Ytmp= []
+    var table = tablevalue
+    const x = table.map(row => row.slice(0, -1).map(Number));
+    const y = table.map(row => row.slice(-1).map(Number));
+    console.log("x", x);
+    console.log("y", y);
+    console.log("Xpredict",xpredict)
+    // Search for a polynomial model of degree = 2.
+    const model = new PolynomialRegressor(10);
+    model.fit(x, y); // Training
+    console.log("tt", model.predict([xpredict]));
 
-      for(let i = 0 ; i < n ;i++){
-          for(let j = m-1 ; j > 0 ; j --){
-              var tmp = 0
-              console.log(`j=${j} :${regres[j]} * ${x[i]}`)
-              tmp += Math.pow(regres[j],j) * x[i]
-            }
-          Ytmp.push(tmp)
-      }
-      setYpredict(Ytmp)
-      console.log("ypredict",ypredict)
-    }
   }
+  const xcreate = () => {
+    const cols = [];
+    for (let j = 0; j < m-1; j++) {
+      cols.push(
+        <Col
+          key={j}
+          xs={12}
+          className="d-flex justify-content-center align-items-center"
+          onChange={(e) => {
+            Setx(e, j);
+          }}
+        >
+          <Form.Control type="number" />
+        </Col>
+      );
+    }
+    setXp(cols);
+  }
+  
+  
     
 
   
@@ -118,24 +127,31 @@ function Test() {
           <Form.Label>N</Form.Label>
           <Form.Control type="number" placeholder="Input N"  onChange={Setn} />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>X</Form.Label>
-          <Form.Control type="number" placeholder="Input X"  onChange={Setx} />
-        </Form.Group>
-        <Button variant="primary" type="button" onClick={createTable}>
+        <Button variant="primary" type="button" onClick={()=>{
+            xcreate();
+            createTable()}}>
           Create Table
         </Button>
+       
+        {/* <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Degree</Form.Label>
+          <Form.Control type="number" placeholder="Input Degree"  onChange={Setdegree} />
+        </Form.Group> */}
+        { havedata > 0 && <Form>
+            <Form.Label>X </Form.Label>
+            {xp}</Form>}
         <Form>
         <br></br>
           {table}
         </Form>
         <br></br>
-        {havedata > 0  && <div><Button variant="primary" type="button" onClick={Calculate}> Calculate</Button>
+        {havedata > 0  && <div>
+            <Button variant="primary" type="button" onClick={Calculate}> Calculate</Button>
         <Form>
             <br></br>
             <Form.Label><h1>{result}</h1></Form.Label>
             <Row className="justify-content-center" >
-            <Plottest  X={plotx} Y={ploty} result={result} predicty={ypredict} fx={X} table={tablevalue} A={A}/>
+            {/* <Plottest  X={plotx} Y={ploty} result={result} predicty={ypredict} fx={X} table={tablevalue} A={A}/> */}
             </Row>
             
         </Form>
